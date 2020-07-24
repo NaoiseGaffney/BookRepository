b5ef437404cf7a4087796a5bd921a29b524b9c18
Naoise Olof Seán Gaffney <61478687+NaoiseGaffney@users.noreply.github.com>
2020-07-24 19:28:29 +0100

    Configured the Continuous Development and Continuous Deployment workflow/lifecycle with GitHub, Visual Studio Code, and Heroku.

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
+import os
+import gunicorn
 from flask import Flask
+
+from dotenv import load_dotenv
+from pathlib import Path
+env_path = Path(".") / (".env")
+load_dotenv(dotenv_path=env_path)
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    if os.environ.get("PRODUCTION") == "ON":
+        return "Hello, World! This Flask application is running in PRODUCTION (debug=False)."
+    else:
+        return "Hello, World! This Flask application is running in DEVELOPMENT or STAGING (debug=True)."
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if os.environ.get("PRODUCTION") == "ON":
+        app.run(host=os.environ.get("IP"),
+                port=os.environ.get("PORT"), debug=False)
+    else:
+        app.run(host=os.environ.get("IP"),
+                port=os.environ.get("PORT"), debug=True)
