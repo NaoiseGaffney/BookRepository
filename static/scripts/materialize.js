ed3f842f08bf8308b6d81bb9fd3d97e2e81f0b63
Naoise Olof Seán Gaffney <61478687+NaoiseGaffney@users.noreply.github.com>
2020-07-31 02:11:27 +0100

    Created new Book Respository Logo. Added "ego-image" too for the about footer link (open book image). Corrected 'materialize.js' issue with two of the same initialisations for the datepicker. Applying colour scheme to the Book Repository. 'index.html', using an H6 instead of an H1 for the page descriptors as it's more inline wiht the rest of the text. 'flask_user_layout.html', Added a fixed/static link to 'style.css' as I wasn't sure it loaded properly. Removed placeholder code, and replaced it with a proper MaterializeCSS naviagtion bar and hamburger-menu for smaller screens. Updated all the url_for endpoints (might have done this earlier). Added correct material icons to buttons and links (good visual practice to combine both text and an image). Created a new footer, fixed footer ("vanilla" CSS), and using MaterializeCSS footer-page and modal for the about (open book icon) footer link. Had previsouly forgotten to add the MaterializeCSS 1.0.0 JavaScript CDN.

--- static/scripts/materialize.js
+++ static/scripts/materialize.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     M.AutoInit(); /* Can't pass in options with M.AutoInit(), only use default values */
-    let elemsDatepicker = document.querySelectorAll(".datepicker");
+    /* let elemsDatepicker = document.querySelectorAll(".datepicker");
     let instanceDatepicker = M.Datepicker.init(elemsDatepicker, {
         yearRange: 3,
         format: 'yyyy-mm-dd'
     });
-    /* let elemsSidenav = document.querySelectorAll(".sidenav");
+    let elemsSidenav = document.querySelectorAll(".sidenav");
     let instancesSidenav = M.Sidenav.init(elemsSidenav); // Remove "options" from (elems, options)
     let elemsMaterialboxed = document.querySelectorAll(".materialboxed");
     let instancesMaterialboxed = M.Materialbox.init(elemsMaterialboxed); // Remove "options" from (elems, options)
@@ -18,11 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let elemsScrollspy = document.querySelectorAll(".scrollspy");
     let instanceScrollspy = M.ScrollSpy.init(elemsScrollspy, {
         throttle: 10
-    });
-    let elemsDatepicker = document.querySelectorAll(".datepicker");
-    let instanceDatepicker = M.Datepicker.init(elemsDatepicker, {
-        disableWeekends: true,
-        yearRange: 3
     });
     let elemsModal = document.querySelectorAll(".modal");
     let instancesModal = M.Modal.init(elemsModal, {
